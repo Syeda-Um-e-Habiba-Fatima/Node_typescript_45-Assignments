6354f0b25b3cb996dc1ab57d24a27cd818bba5b9
Habibihere4 <syedasifshah.2006@gmail.com>
2024-03-23 22:55:34 -0700

    first commit

--- 2_Personal_message/personal_message.js
+++ 2_Personal_message/personal_message.js
@@ -2,8 +2,8 @@
 let personName = '';
 personName = prompt('What is your name?') || '';
 if (personName !== null && personName !== '') {
-    alert(`Hello ${personName}, would you like to learn some python today?`);
+    alert(`Hello ${personName}, Would you like to learn some python today?`);
 }
 else {
-    alert('You have to fill your name !');
+    alert(`You have to fill your name !`);
 }
---
+++ 2_Personal_message/personal_message.ts
@@ -0,0 +1,10 @@
+let personName :string = '';
+
+personName =prompt('What is your name?') || '';
+
+if(personName !== null && personName !== ''){
+    alert(`Hello ${personName}, Would you like to learn some python today?`)
+}
+    else{
+    alert(`You have to fill your name !`)
+}
